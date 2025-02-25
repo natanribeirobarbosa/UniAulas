8ccc40ba35e4f4869167ab595e2bd75ce894815b
Natã Ribeiro Barbosa <95133483+natanribeirobarbosa@users.noreply.github.com>
2025-02-25 19:57:14 -0300

    mudei a cor

--- app/routes/dashboard/page.js
+++ app/routes/dashboard/page.js
@@ -81,7 +81,7 @@ const Dashboard = () => {
   
 
   <Link href="/">
-    <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
+    <button className="bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
     Tela inicial
   </button>
   
